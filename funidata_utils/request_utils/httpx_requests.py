4e49cac248a02ed26eb4b84677a04b1f689eab07
EssKayz <esskay@protonmail.com>
2025-07-11 12:43:41 +0300

    BS-51: Changes from LABP project

--- funidata_utils/request_utils/httpx_requests.py
+++ funidata_utils/request_utils/httpx_requests.py
@@ -1,8 +1,7 @@
 from typing import Tuple, Any, Callable, Literal
 
 import httpx
-
-from src.utils import flatten, group_by
+from ..utils import flatten, group_by
 
 
 ACCEPTED_RESPONSE_CODES = {200, 201, 202, 204}
