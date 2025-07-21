efecd43ae3893f9549941fd53f0655ef9b05dfcb
EssKayz <esskay@protonmail.com>
2025-07-21 13:28:32 +0300

    BS-51: Refactor auth package

--- funidata_utils/auth/__init__.py
+++ funidata_utils/auth/__init__.py
@@ -2,5 +2,7 @@
 #  All rights reserved.
 # ------------------------------------------------------------------------------
 
-from .sis_credentials import SisuSettings
-from .credentials_manager import get_sis_settings
+from .sis_auth import SisuConfig
+from .aws_auth import (AwsConfig, AwsProfileConfig, AwsAccesKeyConfig, EmptyAwsConfig)
+from .database_auth import (DatabaseConfig, MariaDbConfig, MockDbConfig)
+from .basic_auth import BasicAuthUser
