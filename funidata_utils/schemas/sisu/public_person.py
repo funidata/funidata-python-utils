c8a84693a835229a175d5bef45d77a9ce72517f9
Matti Räsänen <matti.rasanen@funidata.fi>
2025-12-12 13:39:39 +0200

    Changed base model to sisbase

--- funidata_utils/schemas/sisu/public_person.py
+++ funidata_utils/schemas/sisu/public_person.py
@@ -1,11 +1,11 @@
 #  Copyright (c) 2025 Funidata Oy.
 #  All rights reserved.
 # ------------------------------------------------------------------------------
-from pydantic import BaseModel, conlist
-
+from pydantic import conlist
+from funidata_utils.schemas.sisu.base import SisBase
 from .common import LocalizedString, OTM_ID_REGEX_VALIDATED_STR, SIS_MAX_SMALL_SET_SIZE
 
-class PublicPerson(BaseModel):
+class PublicPerson(SisBase):
     id: str = None
     universityOrgIds: conlist(OTM_ID_REGEX_VALIDATED_STR, max_length=SIS_MAX_SMALL_SET_SIZE)
     titles: LocalizedString | None = None
