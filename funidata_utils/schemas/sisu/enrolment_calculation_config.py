2b86c234cc6ee39118fe6d4bc912259435c5bd5e
rrimpila <raine.rimpila@funidata.fi>
2026-03-27 16:43:29 +0200

    LABP-277 add serializers for set fields

--- funidata_utils/schemas/sisu/enrolment_calculation_config.py
+++ funidata_utils/schemas/sisu/enrolment_calculation_config.py
@@ -3,8 +3,9 @@
 # ------------------------------------------------------------------------------
 from typing import Annotated, Literal, Union
 
-from pydantic import BaseModel, Field, conset
+from pydantic import BaseModel, Field, conset, field_serializer
 
+from funidata_utils.schemas.common_serializers import serialize_as_list
 from funidata_utils.schemas.sisu.base import SisBase
 from funidata_utils.schemas.sisu.common import (
     OTM_ID_REGEX_VALIDATED_STR, LocalizedString, STRIPPED_STR, sis_code_urn_pattern, CreditRange,
@@ -19,6 +20,11 @@ class AttainedCreditRangeRule(BaseModel):
     creditRange: CreditRange | None = None
     creditOrder: Literal['NONE', 'ASCENDING', 'DESCENDING']
 
+    @field_serializer("educationIds")
+    def serialize_set_as_list(self, v, _info) -> list[str] | None:
+        serialized_list = serialize_as_list(v)
+        return serialized_list
+
 
 class CompulsoryFormalPrerequisitesRule(BaseModel):
     type: Literal['CompulsoryFormalPrerequisites'] = 'CompulsoryFormalPrerequisites'
@@ -32,15 +38,30 @@ class EducationRule(BaseModel):
     type: Literal['Education'] = 'Education'
     educationGroupIds: conset(OTM_ID_REGEX_VALIDATED_STR, min_length=1)  # noqa
 
+    @field_serializer("educationGroupIds")
+    def serialize_set_as_list(self, v, _info) -> list[str] | None:
+        serialized_list = serialize_as_list(v)
+        return serialized_list
+
 
 class EducationTypeRule(BaseModel):
     type: Literal['EducationType'] = 'EducationType'
     educationTypeUrns: conset(Annotated[STRIPPED_STR, Field(pattern=sis_code_urn_pattern('education-type'))], min_length=1)  # noqa
 
+    @field_serializer("educationTypeUrns")
+    def serialize_set_as_list(self, v, _info) -> list[str] | None:
+        serialized_list = serialize_as_list(v)
+        return serialized_list
+
 
 class EnrolmentForCourseUnitRealisationRule(BaseModel):
     type: Literal['EnrolmentForCourseUnitRealisation'] = 'EnrolmentForCourseUnitRealisation'
     courseUnitRealisationIds: conset(OTM_ID_REGEX_VALIDATED_STR, min_length=1)  # noqa
+
+    @field_serializer("courseUnitRealisationIds")
+    def serialize_set_as_list(self, v, _info) -> list[str] | None:
+        serialized_list = serialize_as_list(v)
+        return serialized_list
 
 
 class NotAlreadyEnrolledToAssessmentItemRule(BaseModel):
@@ -50,6 +71,11 @@ class NotAlreadyEnrolledToAssessmentItemRule(BaseModel):
 class PersonGroupMembershipRule(BaseModel):
     type: Literal['PersonGroupMembership'] = 'PersonGroupMembership'
     personGroupIds: conset(OTM_ID_REGEX_VALIDATED_STR, min_length=1)  # noqa
+
+    @field_serializer("personGroupIds")
+    def serialize_set_as_list(self, v, _info) -> list[str] | None:
+        serialized_list = serialize_as_list(v)
+        return serialized_list
 
 
 class RecommendedFormalPrerequisitesRule(BaseModel):
