7d3e9b25395c1ae937f88b1d4e37f9abf6065874
rrimpila <raine.rimpila@funidata.fi>
2026-04-02 15:34:43 +0300

    LABP-283 Reformat

--- funidata_utils/schemas/sisu/code_book.py
+++ funidata_utils/schemas/sisu/code_book.py
@@ -17,14 +17,16 @@ class Code(SisBase):
     urn: Annotated[STRIPPED_STR, Field(pattern=sis_code_urn_pattern('.+'))]
     parentUrn: Annotated[STRIPPED_STR, Field(pattern=sis_code_urn_pattern('.+'))] | None
     isLeafNode: bool | None
-    universitySpecifier: str | None # Literal['HY', 'JYU', 'LUT', 'TUNI', 'AALTO', 'ARC', 'SHH'] | None but I really don't want to keep updating this...
+    universitySpecifier: str | None  # Literal['HY', 'JYU', 'LUT', 'TUNI', 'AALTO', 'ARC', 'SHH'] | None but I really don't want to keep updating this...
     deprecated: bool | None
     exceptionalVirtaValue: str | None
     type: Literal['Code']
 
+
 class CompetencyCode(Code):
     type: Literal['CompetencyCode']
     credits: int | None
+
 
 class CountryCode(Code):
     type: Literal['CountryCode']
@@ -32,12 +34,14 @@ class CountryCode(Code):
     alpha2: str | None
     alpha3: str | None
 
+
 class CodeBook(SisBase):
     documentState: Literal['ACTIVE', 'DRAFT', 'DELETED']
     urn: Annotated[STRIPPED_STR, Field(pattern=sis_code_urn_pattern('.+'))]
     codeBookType: Literal['COMMON', 'CUSTOM'] | None
     name: LocalizedString
-    classificationScopeUrns: conset(Annotated[STRIPPED_STR, Field(pattern=sis_code_urn_pattern('classification-scope'))], max_length=SIS_MAX_SMALL_SET_SIZE) | None  # noqa
+    classificationScopeUrns: conset(Annotated[STRIPPED_STR, Field(pattern=sis_code_urn_pattern('classification-scope'))],
+                                    max_length=SIS_MAX_SMALL_SET_SIZE) | None  # noqa
     universityOrgIds: conlist(OTM_ID_REGEX_VALIDATED_STR, max_length=1) | None  # noqa
     codeVisibility: Literal['ALWAYS_VISIBLE', 'HIDING_CODES_ALLOWED']
     codes: conset(Code | CompetencyCode | CountryCode, min_length=1, max_length=SIS_MAX_VERY_LARGE_SET_SIZE)  # noqa
