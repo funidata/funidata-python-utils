e8dd4ba279d2d07f2af3dcb5cc5c9bb9b7bb9fa5
EssKayz <esskay@protonmail.com>
2026-09-04 11:50:46 +0300

    BS-170: Osuva plan scrambler

--- funidata_utils/sis_integration/resources/sis_resources.py
+++ funidata_utils/sis_integration/resources/sis_resources.py
@@ -6,6 +6,7 @@ from re import sub
 
 from funidata_utils.data_scramblers.private_person import PrivatePersonScrambler
 from ...data_scramblers.study_right import StudyRightScrambler
+from ...data_scramblers.osuva_plan_scrambler import OsuvaPlanScrambler
 
 
 _DEFAULT_EXPORT_LIMIT = 2500
@@ -418,6 +419,9 @@ class CooperationNetworks(BaseResource):
 
 
 class OsuvaPlans(BaseResource):
+    scrambling_classes = [
+        OsuvaPlanScrambler
+    ]
     imports = SisImport(
         endpoint='/osuva/api/plans/v1/import',
         default_import_limit=_DEFAULT_IMPORT_LIMIT,
