d46ca815c4a910d5258cc2aa22df062e604455dc
xtooth <jamsen.kim@gmail.com>
2026-09-17 09:56:30 +0300

    BS-163: fix dict hashing bug and update gitignore

--- funidata_utils/utils.py
+++ funidata_utils/utils.py
@@ -159,7 +159,7 @@ def _dict_update_by_key_split(
         if isinstance(new_value, Callable):
             _new_value = new_value(_ref)
         elif isinstance(new_value, tuple):
-            _new_value = new_value[0](str(_ref), **new_value[1])
+            _new_value = new_value[0](_ref, **new_value[1])
         else:
             _new_value = new_value
 
