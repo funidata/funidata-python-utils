b950c9bfa1c539117e6bb1716654607db186af36
rrimpila <raine.rimpila@funidata.fi>
2026-09-21 12:20:07 +0300

    BS-173 Update tuition fee obligation period test name

--- tests/scramblers/ori/test_tuition_fee_obligation_period_scrambler.py
+++ tests/scramblers/ori/test_tuition_fee_obligation_period_scrambler.py
@@ -6,7 +6,7 @@ from funidata_utils.data_scramblers.tuition_fee_obligation_period import Tuition
 
 
 @pytest.mark.unit
-def test_tuition_fee_scrambler_scrambles_additional_info():
+def test_tuition_fee_scrambler_scrambles():
     data = {
         "id": "otm-123456",
         "studyRightId": "otm-123456",
